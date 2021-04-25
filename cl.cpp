3a0aa484328befd31f10f57b06f215c408b99319
adrina-ei <adrina.ei24@gmail.com>
2021-04-25 10:22:45 +0430

    sobhoone nkhorde

--- cl.cpp
+++ cl.cpp
@@ -57,7 +57,7 @@ public:
 	void push_befor(int data);
         void print()
         {
-            print << "what the hell it is? chikarsh bknim?" << endl;
+            cout << "what the hell it is? chikarsh bknim?" << endl;
         }
 };
 
