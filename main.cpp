d201fbac14b195db130ff6652c50547ec9fdf6a0
adrina-ei <adrina.ei24@gmail.com>
2021-05-10 14:27:07 +0430

    Adri's main?

--- main.cpp
+++ main.cpp
@@ -174,7 +174,160 @@ public:
     }
 };
 
-int main()
+string action_menu()
 {
-    Circular_linked_list aka;
+    cout << "Hi buddy! What do u wanna do?" << endl;
+
+    cout << "1.Make a linkl list(Your very first step)\n"
+         << "2.Get the size of your current link list\n"
+         << "3.Push data in current place\n"
+         << "4.Pop current place\n"
+         << "5.Find a special data\n"
+         << "6.Pop a special data\n"
+         << "7.Push after current\n"
+         << "8.Push before current\n"
+         << "9.Print your link list\n"
+         << "To exit the program enter \"Let me out of this shit\"" << endl;
+
+    string action;
+    getline(cin,action);
+    return action;
+}
+
+Circular_linked_list aka;
+
+int main()
+{  
+    string action = action_menu();
+    if(action == "1")
+    {
+        cout << "Your link list is ready dude!" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "2")
+    {
+        cout << aka.getSize() << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "3")
+    {
+        cout << "\n               --------------------------------------\n"
+             << "Enter the data you wanna push: " ;
+        int d;
+        cin >> d;
+        aka.push(d);
+        cout << endl << "Your data successfullu pushed :)" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "4")
+    {
+        cout << "\n               --------------------------------------\n";
+        if(aka.pop() == true)
+            cout << endl << "Your current data successfully poped :)" << endl;
+        else
+            cout << "Unreachable request! Ye chi bkha ke beshe :/" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "5")
+    {
+        cout << "\n               --------------------------------------\n"
+             << "Enter the data you wanna seek for: " ;
+        int d;
+        cin >> d;
+        if(aka.search() != nullptr)
+            cout << endl << "Your current data successfully found :)" << endl;
+        else
+            cout << "Unreachable request! Bebin karato :/" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "6")
+    {
+        cout << "\n               --------------------------------------\n"
+             << "Enter the data you wanna pop: " ;
+        int d;
+        cin >> d;
+        if(aka.pop == true)
+            cout << endl << "Your data successfully poped :)" << endl;
+        else
+            cout << "Unreachable request! Ish Ish :/" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "7")
+    {
+        cout << "\n               --------------------------------------\n"
+             << "Enter the data you wanna push after current data: " ;
+        int d;
+        cin >> d;
+        aka.push_after(d);
+        cout << endl << "Your data successfullu pushed :)" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "8")
+    {
+        cout << "\n               --------------------------------------\n"
+             << "Enter the data you wanna push before current data: " ;
+        int d;
+        cin >> d;
+        aka.push_before(d);
+        cout << endl << "Your data successfullu pushed :)" << endl << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "9")
+    {
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||Here is your link list!|||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        aka.print();
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||" << endl;
+        cout << endl;
+        cout << "Press Enter to go back to main menu.";
+        string enter;
+        cin >> enter;
+        if(enter == "\n")
+            main();
+    }
+    else if(action == "Let me out of this shit")
+    {
+        cout << "Goodbye! Beri ke barnagardi :)" << endl;
+        return 0;
+    }
+
 }
